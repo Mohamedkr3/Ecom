c59184d507cac1068875bd9a23c3a02a345053e9
Mohamed khaled Rezk <101335766+Mohamedkr3@users.noreply.github.com>
2024-08-27 23:11:24 +0300

    Add files via upload

---
+++ src/components/heroSection/HeroSection.jsx
@@ -0,0 +1,9 @@
+const HeroSection = () => {
+    return (
+        <div>
+           <img className="h-40 lg:h-full" src="/pic.png" alt="Hero Section Image" />
+        </div>
+    );
+}
+
+export default HeroSection;
---
+++ src/components/testimonial/Testimonial.jsx
@@ -0,0 +1,54 @@
+/* eslint-disable react/no-unescaped-entities */
+
+const Testimonial = () => {
+    return (
+        <div>
+            <section className="text-gray-600 body-font mb-10">
+                {/* main  */}
+                <div className="container px-5 py-10 mx-auto">
+                    {/* Heading  */}
+                    <h1 className=' text-center text-3xl font-bold text-black' >Feed Back</h1>
+                    {/* para  */}
+                    <h2 className=' text-center text-2xl font-semibold mb-10' >What our <span className=' text-blue-500'>customers</span> are saying</h2>
+
+                    <div className="flex flex-wrap -m-4">
+                        {/* Testimonial 1 */}
+                        <div className="lg:w-1/3 lg:mb-0 mb-6 p-4">
+                            <div className="h-full text-center">
+                                <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://ecommerce-sk.vercel.app/img/kamal.png" />
+                                <p className="leading-relaxed">The user experience on this site is fantastic. The layout is clean, and I found exactly what I was looking for within minutes. Plus, the currency in EGP made it convenient for me as an Egyptian buyer."</p>
+                                <span className="inline-block h-1 w-10 rounded bg-blue-500 mt-6 mb-4" />
+                                <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm uppercase">Kamal Ahmed</h2>
+                                <p className="text-gray-500">Senior Product Designer</p>
+                            </div>
+                        </div>
+
+                        {/* Testimonial 2 */}
+                        <div className="lg:w-1/3 lg:mb-0 mb-6 p-4">
+                            <div className="h-full text-center">
+                                <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://www.devknus.com/img/gawri.png" />
+                                <p className="leading-relaxed">I was amazed by the quality of the products! The detailed descriptions and high-resolution images made shopping so much easier. I will definitely be a returning customer."</p>
+                                <span className="inline-block h-1 w-10 rounded bg-blue-500 mt-6 mb-4" />
+                                <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm uppercase">S Mishra</h2>
+                                <p className="text-gray-500">UI Develeoper</p>
+                            </div>
+                        </div>
+
+                        {/* Testimonial 3 */}
+                        <div className="lg:w-1/3 lg:mb-0 p-4">
+                            <div className="h-full text-center">
+                                <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://firebasestorage.googleapis.com/v0/b/devknus-official-database.appspot.com/o/images%2FScreenshot%202023-07-07%20at%202.20.32%20PM-modified.png?alt=media&token=324ddd80-2b40-422c-9f1c-1c1fa34943fa" />
+                                <p className="leading-relaxed">Great variety of products at reasonable prices. The seamless checkout process and quick delivery added to my satisfaction. Highly recommend!"</p>
+                                <span className="inline-block h-1 w-10 rounded bg-blue-500 mt-6 mb-4" />
+                                <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm uppercase">XYZ </h2>
+                                <p className="text-gray-500">CTO</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Testimonial
